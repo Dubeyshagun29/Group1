69f45326d3aa0192025b1e8e6ea3c43354c10066
Dubey <Shagun@CyberLab.com>
2022-03-22 10:42:58 +0530

    done

--- Login.java
+++ Login.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
     String Password;
 
     Password = "123";
-    Username = "Ranger";
+    Username = "AppSec";
 
     Scanner input1 = new Scanner(System.in);
     System.out.println("Enter Username : ");
