d72b2982a3b49957c25b255959bdf2b60d1ad8b5
Dubeyshagun29 <Dubey.shagun29@gmail.com>
2022-03-28 17:12:24 +0530

    commit

--- Demo1/src/User_Authentication.java
+++ Demo1/src/User_Authentication.java
@@ -20,7 +20,7 @@ public class User_Authentication
 
         password = s.nextLine();
 
-        if(username.equals("admin222") && password.equals("user123"))
+        if(username.equals("admin22") && password.equals("user123"))
 
         {
 
